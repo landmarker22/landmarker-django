5c8957db11b8f1b5bb2b00b77fe941dab964116f
soobin <soobin512@naver.com>
2022-04-26 02:41:10 +0900

    psb 갤러리 detail

--- Model/gallery/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Feed(models.Model):
-    content = models.TextField()
-    image = models.TextField()
-    profile_image = models.TextField()
-    user_id = models.TextField()
-    like_count = models.IntegerField()
