f0eb21e1f14219d8d8589d0181f0752ab61aa532
soobin <soobin512@naver.com>
2022-04-30 18:50:53 +0900

    psb 갤러리 좋아요, 검색 기능 완료

--- Model/gallery/gallery_controller.py
+++ Model/gallery/gallery_controller.py
@@ -4,13 +4,20 @@ import cx_Oracle
 import common.oracle_db as odb
 import Model.gallery.gallery_class as gclass
 
-def select_all(user):
+def select_all(user, ob):
     conn = odb.connect()
     cursor = None
-    query = 'SELECT * ' \
-            'FROM L_GALLERY ' \
-            'JOIN L_USER USING (USER_NO)'
     gallery_list = []
+    if ob == 0:
+        query = 'SELECT * ' \
+                'FROM L_GALLERY ' \
+                'JOIN L_USER USING (USER_NO) ' \
+                'ORDER BY GALLERY_DATE DESC'
+    else:
+        query = 'select gallery_no, count(*) c \
+                from l_like \
+                group by gallery_no \
+                ORDER by c desc'
 
     try:
         cursor = conn.cursor()
@@ -33,7 +40,6 @@ def select_all(user):
 
     except Exception as msg:
         print('Gallery select_all() 에러 발생 : ', msg)
-
 
     query = 'SELECT * ' \
             'FROM L_LIKE ' \
@@ -65,7 +71,7 @@ def select_all(user):
     print('gallery_list : ', gallery_list)
     return gallery_list
 
-def select_one(g_no):
+def select_one(g_no, u_no):
     detail_list = []
     comment_list = []
     conn = odb.connect()
@@ -111,6 +117,28 @@ def select_one(g_no):
         print('Detail select_one() detail 에러 발생 : ', msg)
 
     query = 'SELECT COUNT(*) ' \
+            'FROM L_LIKE ' \
+            'WHERE GALLERY_NO = ' + g_no
+
+    try:
+        cursor = conn.cursor()
+        like_count = cursor.execute(query).fetchone()
+
+    except Exception as msg:
+        print('Detail select_one() like_count 에러 발생 : ', msg)
+
+    query = 'SELECT COUNT(*) ' \
+            'FROM L_LIKE ' \
+            'WHERE GALLERY_NO = ' + g_no + ' AND USER_NO = ' + str(u_no)
+
+    try:
+        cursor = conn.cursor()
+        like = cursor.execute(query).fetchone()
+
+    except Exception as msg:
+        print('Detail select_one() like_count 에러 발생 : ', msg)
+
+    query = 'SELECT COUNT(*) ' \
             'FROM L_COMMENT ' \
             'JOIN L_USER USING (USER_NO) ' \
             'WHERE GALLERY_NO = ' + g_no
@@ -133,5 +161,73 @@ def select_one(g_no):
 
     # print('detail_list : ', detail_list)
     # print('comment_list : ', comment_list)
-    return detail, comment_list, c_count_result
+    return detail, comment_list, like_count, like, c_count_result
     # return detail_result, comment_result, c_count_result
+
+def gallike(g_no, u_no, onoff):
+    conn = odb.connect()
+    cursor = None
+
+    if onoff == '1':
+        query = 'DELETE FROM L_LIKE ' \
+                'WHERE GALLERY_NO = ' + g_no + ' AND USER_NO = ' + u_no
+        try:
+            cursor = conn.cursor()
+            cursor.execute(query)
+            conn.commit()
+        except Exception as msg:
+            print('Gallery like() off 에러 발생 : ', msg)
+        finally:
+            cursor.close()
+            odb.close(conn)
+
+    else:
+        query = 'INSERT INTO L_LIKE ' \
+                'VALUES(' + u_no + ', ' + g_no + ')'
+        try:
+            cursor = conn.cursor()
+            cursor.execute(query)
+            conn.commit()
+        except Exception as msg:
+            print('Gallery like() on 에러 발생 : ', msg)
+        finally:
+            cursor.close()
+            odb.close(conn)
+
+    return '좋아요 변경됨'
+
+def search(s):
+    conn = odb.connect()
+    cursor = None
+    search_list = []
+
+    query = 'SELECT * ' \
+            'FROM L_GALLERY ' \
+            'JOIN L_USER USING (USER_NO)' \
+            "WHERE HASHTAG LIKE '%" + s + "%' " \
+            "ORDER BY GALLERY_DATE DESC"
+    try:
+        cursor = conn.cursor()
+        result = cursor.execute(query)
+
+        for row in result:
+            print(type(row[6].strftime('%Y-%m-%d, %H:%M:%S')))
+            row_dict = {'g_no': row[1], 'u_no': row[0], 'content': row[2],
+                        'photopath': row[3], 'hashtag': row[4], 'rcount': row[5],
+                        'date': row[6].strftime('%Y-%m-%d %H:%M:%S'), 'u_name': row[8]}
+            print('row_dict : ', row_dict)
+
+            # ref = gclass.Gallery(row_dict)  # Gallery 객체 생성
+            # print('gclass.Gallery.info(ref) : ', gclass.Gallery.info(ref))
+            # gallery_list.append(ref)
+
+            search_list.append(row_dict)
+
+    except Exception as msg:
+        print('Gallery search() 에러 발생 : ', msg)
+    finally:
+        cursor.close()
+        odb.close(conn)
+
+    print(search_list)
+    return search_list
--- View/gallery/gallery.py
+++ View/gallery/gallery.py
@@ -11,7 +11,7 @@ import Model.gallery.gallery_controller as gcontroller
 
 
 def gallery(request):
-    data = gcontroller.select_all(7)
+    data = gcontroller.select_all(7, 0)
     print(data)
     context = {
         'head': 'parts/head.html',
@@ -25,12 +25,36 @@ def gallery(request):
 
 def gdetail(request):
     print('상세게시글번호 : ', request.GET['g_no'])
-    data = gcontroller.select_one(request.GET['g_no'])
+    data = gcontroller.select_one(request.GET['g_no'], 7)
     print('상세게시글 data : ', data)
     context = {
         'detail': data[0],
         'comment': data[1],
-        'c_count': data[2][0]
+        'like_count': data[2][0],
+        'like': data[3][0],
+        'c_count': data[4][0]
     }
     # return HttpResponse(json.dumps(data), content_type='application/json')
     return render(request, 'common/gdetail.html', context)
+
+def gallike(request):
+    data = gcontroller.gallike(request.GET['g_no'], request.GET['u_no'], request.GET['onoff'])
+    print(data)
+    context = {
+        'head': 'parts/head.html',
+        'navi': 'parts/navi.html',
+        'foot': 'parts/foot.html',
+        'footer': 'parts/footer.html',
+        'data': data,
+    }
+    return render(request, 'common/gallery.html', context)
+
+def galsearch(request):
+    print('검색어 : ', request.GET['search'])
+    data = gcontroller.search(request.GET['search'])
+    print('상세게시글 data : ', data)
+    context = {
+        'data': data,
+    }
+    # return HttpResponse(json.dumps(data), content_type='application/json')
+    return render(request, 'common/gsearch.html', context)
