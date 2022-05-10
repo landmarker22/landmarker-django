2daa5e4aab2eb71bd06895881bece827b42a7a79
jejjej97 <jejjej97@naer.com>
2022-05-10 14:44:04 +0900

    ai

--- View/photoSearch/photoSearchView/photoSearchView.py
+++ View/photoSearch/photoSearchView/photoSearchView.py
@@ -1,3 +1,5 @@
+import random
+
 from django.shortcuts import render
 import json
 import os
@@ -15,11 +17,11 @@ def photoSearch(request):
     return render(request, 'photoSearch/photoSearch.html', context)
 
 def photoSearch_ai(request):
-
+    num = random.randrange(1000000000, 9999999999)
     img = request.FILES.get("uploadFiles")
     print(type(img))
 
-    imgname = img._name
+    imgname = str(num)+"_"+img._name
     f = open('%s/%s' % (os.path.join(BASE_DIR,"static/AI_img"), imgname), 'wb')
     for chunk in img.chunks():
         f.write(chunk)
